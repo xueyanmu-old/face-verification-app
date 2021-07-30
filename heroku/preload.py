426f49151a6b46037c019e7124e392c9e56f929e
XueyanMu <xm14@rice.edu>
2021-07-30 16:12:14 -0300

    w

--- heroku/preload.py
+++ heroku/preload.py
@@ -1,16 +1,16 @@
-import tensorflow as tf
-from tensorflow.keras import preprocessing
-from PIL import Image
-from io import BytesIO
-from deepface import DeepFace
-
-with open("image.jpeg", "rb") as image:
-    f = image.read()
-    b = bytearray(f)
-    img = Image.open(BytesIO(b))
-image_array = tf.keras.preprocessing.image.img_to_array(img)
-
-demography = DeepFace.analyze(image_array, actions=["emotion"])
-
-result = DeepFace.verify(image_array, image_array, model_name="Facenet")
-print("Is verified: ", result["verified"])
+# import tensorflow as tf
+# from tensorflow.keras import preprocessing
+# from PIL import Image
+# from io import BytesIO
+# from deepface import DeepFace
+#
+# with open("image.jpeg", "rb") as image:
+#     f = image.read()
+#     b = bytearray(f)
+#     img = Image.open(BytesIO(b))
+# image_array = tf.keras.preprocessing.image.img_to_array(img)
+#
+# demography = DeepFace.analyze(image_array, actions=["emotion"])
+#
+# result = DeepFace.verify(image_array, image_array, model_name="Facenet")
+# print("Is verified: ", result["verified"])
