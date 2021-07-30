4f232e96abda0369494042da0a0985ec68899416
XueyanMu <xm14@rice.edu>
2021-07-30 13:15:11 -0300

    back to master ubuntu latest"

---
+++ server/main/toRGB.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from deepface import DeepFace
+import tensorflow as tf
+
+image1 = Image.open('gid140.png')
+img1 = image1.convert('RGB')
+rgb1 = img1.save("rbg1.jpg")
+img1 = tf.keras.preprocessing.image.img_to_array(img1)
+
+image2 = Image.open('Gface.jpg')
+img2 = image2.convert('RGB')
+rgb2 = img2.save("rbg2.jpg")
+img2 = tf.keras.preprocessing.image.img_to_array(img2)
+#
+# # save to database
+# instance = compare()
+# instance.first.put(io.BytesIO(file[0]))
+# instance.second.put(io.BytesIO(file[1]))
+# instance.save()
+
+# try:
+result = DeepFace.verify(img1, img2, model_name="Facenet", enforce_detection=False)
+# except:
+#     result = "L"
+print({"result": result})
